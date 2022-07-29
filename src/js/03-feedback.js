4fda2d2d113f91b5776cf73be185cdaacb5c49ff
Pavel-1988 <102318642+Pavel-1988@users.noreply.github.com>
2022-07-29 13:34:51 +0300

    1я сдача

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,36 +1,7 @@
-// // import throttle from 'lodash.throttle';
-// const form = document.querySelector('.feedback-form');
-// // form.addEventListener('input', throttle(onFormData, 500));
-// form.addEventListener('input', onFormData);
-// form.addEventListener('submit', onFormSubmit);
-
-// const formData = {};
-
-// function onFormData(e) {
-//     formData[e.target.name] = e.target.value;
-//     localStorage.setItem('feedback-form-state', JSON.stringify(formData));
-    
-// }
-
-// function onFormSubmit(e) {
-//     e.preventDefault();
-//     e.currentTarget.reset();
-//     const formElements = e.currentTarget.elements
-//     const mail = formElements.email.value
-//     const message = formElements.message.value
-//     const submitData = {
-//         mail,
-//         message
-//     }
-//     console.log(submitData);
-// }
-
-//==========
-
+import throttle from 'lodash.throttle'
 const form = document.querySelector('.feedback-form');
 const email = document.querySelector('.feedback-form input')
 const message = document.querySelector('.feedback-form textarea')
-
 
 form.addEventListener('input', throttle(onFormData, 500));
 form.addEventListener('submit', onFormSubmit);
@@ -41,7 +12,7 @@ function onFormData(e) {
     // у formData збираємо данні value під name ( тут у нас email та message)
     formData[e.target.name] = e.target.value; 
     // збергіаємо данні у об'єкт де будуть данні з formData( тобто  email та message )
-    localStorage.setItem('feedback-form-state', JSON.stringify(formData));  
+    localStorage.setItem('feedback-form-state', JSON.stringify(formData));     
 }
 
 function onFormSubmit(e) {
@@ -61,5 +32,5 @@ function dataFromLocalStorage() {
         email.value = savedData.email;
         message.value = savedData.message;
     }
- 
 }
+dataFromLocalStorage()
